0398cdacea213d883d17d0e90785dbf43653693f
IvandbNazarov <kjiapku@rambler.ru>
2020-11-01 13:35:51 +0300

    ....

--- ПРОЕКТЫ/Good Garage/js/custom.js
+++ ПРОЕКТЫ/Good Garage/js/custom.js
@@ -7,3 +7,9 @@ $(".navintro").hide();
 $(".nav").on("click", function(){
 	$(".navintro").toggle();
 });
+
+$(".social").hide();
+
+$(".contact li h2").on("click", function(){
+	$(".social").toggle()
+});
